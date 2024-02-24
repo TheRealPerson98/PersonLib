3d2d13dab7e6a623ef07c9bb5d0745e80624f70c
Person98 <jacesleeman603@gmail.com>
2024-02-24 05:38:59 -0500

    v1

--- src/main/java/com/person98/utils/Test.java
+++ src/main/java/com/person98/utils/Test.java
@@ -5,18 +5,6 @@ import org.bukkit.Bukkit;
 import java.util.Properties;
 
 public class Test {
-    public static String getVersion() {
-        String version = "unknown";
-        try {
-            Properties p = new Properties();
-            p.load(Test.class.getResourceAsStream("/META-INF/maven/com.person98/PersonLib/pom.properties"));
-            version = p.getProperty("version");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return version;
-    }
-
     public static String TestPaperAPI() {
         String version = Bukkit.getVersion();
         return "The server is running version: " + version;
